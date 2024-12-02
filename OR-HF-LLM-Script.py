10f529fed7e3288e589df58c256b78ab1f5a705a
Julien Vallières <85407589+archilecteur@users.noreply.github.com>
2024-12-02 01:04:48 -0500

    Update OR-HF-LLM-Script.py

--- OR-HF-LLM-Script.py
+++ OR-HF-LLM-Script.py
@@ -3,7 +3,7 @@ import urllib2
 
 url = 'https://api-inference.huggingface.co/models/{model_lab}/{model_name}/v1/chat/completions'
 api_key = 'hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-basisprompt = u'''Generate a basic JSON containing only the following information on the person mentioned: dateofbirth, placeofbirth, dateofdeath, placeofdeath. Do not provide further information.'''
+prompt = u'''Generate a basic JSON containing only the following information on the person mentioned: dateofbirth, placeofbirth, dateofdeath, placeofdeath. Do not provide further information.'''
 
 headers = {
     'Authorization': 'Bearer ' + api_key,
@@ -14,7 +14,7 @@ data = {
   'messages': [ 
       {
          'role': 'user',
-         'content': basisprompt + value
+         'content': prompt + value
       }
   ],
   'max_tokens': 2048,
